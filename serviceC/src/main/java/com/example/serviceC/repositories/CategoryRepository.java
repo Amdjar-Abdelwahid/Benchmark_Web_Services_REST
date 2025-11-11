fdbd2cd418a9424433d817daca76d35dbb368bf3
Amdjar-Abdelwahid <amdjarabdelwahid1@gmail.com>
2025-11-11 22:14:43 +0100

    done

--- serviceC/src/main/java/com/example/serviceC/repositories/CategoryRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.serviceC.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.example.serviceC.entities.Category;
-
-public interface CategoryRepository extends JpaRepository<Category, Long> {
-}
-
